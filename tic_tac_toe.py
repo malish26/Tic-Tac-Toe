174f4cb088cf1959b8f7d71a1e6cfcf2418e2ff8
Ulyana_Ovcharenko <malish26rus@mail.ru>
2022-11-14 21:03:53 +0300

    1

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -85,10 +85,10 @@ while True:
         screen.fill(black)
         font = pygame.font.S('stxingkai', 80)
         text = font.render(game_over, True, white)
-        text_rect = text1.get_rect()
+        text_rect = text.get_rect()
         text_x = screen.get_width() / 2 - text_rect.width / 2
         text_y = screen.get_height() / 2 - text_rect.height / 2
-        screen.blit(text1, [text_x, text_y])
+        screen.blit(text, [text_x, text_y])
 
      
     pygame.display.update()
